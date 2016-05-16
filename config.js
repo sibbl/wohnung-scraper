891d30d55b72993c010b6d5e6c6de32202f5a113
Sebastian <me@sibbl.net>
2016-05-16 18:25:03 +0200

    Enable multiple page scraping

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 module.exports = {
   database: "wohnungen.db",
+  httpOptions: {
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+    }
+  },
   scraper: {
     wgGesucht: {
-      url: "http://www.wg-gesucht.de/wohnungen-in-Berlin.8.2.0.0.html"
+      url: "http://www.wg-gesucht.de/wohnungen-in-Berlin.8.2.0.0.html",
+      maxPages: 5
     }
   }
 }
