e3bfc1340f2f78fa6e8e99c242ca1121e60acb43
Sebastian <me@sibbl.net>
2016-05-20 22:10:44 +0200

    Add marker to map

---
+++ app/public/script.js
@@ -0,0 +1,61 @@
+var map = L.map('map').setView([52.504703, 13.324861], 12);
+
+var resize = function () {
+  var $map = $('#map');
+
+  $map.height($(window).height());
+
+  if (map) {
+    map.invalidateSize();
+  }
+};
+
+$(window).on('resize', function () {
+  resize();
+});
+
+resize();
+
+var baseLayer = new L.StamenTileLayer('toner', {
+  detectRetina: true
+});
+
+baseLayer.addTo(map);
+
+$.getJSON("/data", function(result) {
+
+  result.forEach(function(wohnung) {
+    if(!wohnung.active) {
+      console.log("inactive: " + wohnung.id);
+      return;
+    }
+    var pricePerSquareMeters = 12;
+    var sizeFunc = new L.LinearFunction([0, 0], [80, 30]); //[input, output]
+    var priceFunc = new L.HSLHueFunction(new L.Point(0, 120), new L.Point(pricePerSquareMeters - 3, 0)); //green (hue of 120) and red (hue of 0)
+    var key = wohnung.price + "€, " + wohnung.size + "m²";
+    var data = {};
+    data[key] = 1;
+    var chartOptions = {};
+    var pricePerSqM = wohnung.price / wohnung.size;
+    var text = wohnung.rooms + "Z, " + pricePerSqM.toFixed(2) + "€/m²";
+    chartOptions[key] = {
+      fillColor: priceFunc.evaluate(Math.abs(pricePerSquareMeters - pricePerSqM)),
+      minValue: 0,
+      maxValue: 600,
+      maxHeight: 20,
+      maxRadius: 20,
+      displayText: function (value) {
+          return text;
+      }
+    };
+    var marker = new L.StackedRegularPolygonMarker(new L.LatLng(wohnung.latitude, wohnung.longitude), {
+        radius: sizeFunc.evaluate(wohnung.size),
+        numberOfSides: (wohnung.rooms + 1),
+        rotation: -90,
+        data: data,
+        chartOptions: chartOptions
+    });
+    marker.addTo(map);
+  });
+});
+
