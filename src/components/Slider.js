fceb29ae6adfd4bc717f91890eb7a1bab94b2f1a
Antje Schubotz <antje.schubotz@gmail.com>
2019-04-28 15:34:56 +0200

    add basic slider component

---
+++ src/components/Slider.js
@@ -0,0 +1,6 @@
+import React from "react";
+import InputRange from "react-input-range";
+import "react-input-range/lib/css/index.css";
+
+const Slider = props => <InputRange draggableTrack {...props} />;
+export default Slider;
