85cbca5bf6f20f6dc03dae0dd5f495096a26de43
Sebastian Müller <me@sibbl.net>
2019-05-05 14:24:59 +0200

    Add hover style to map

--- src/components/flat-map/flat-drawing-helper.js
+++ src/components/flat-map/flat-drawing-helper.js
@@ -1,6 +1,8 @@
 import { line as d3Line } from "d3";
+import { select as d3Select } from "d3-selection";
 import { scaleLinear, scaleSequential } from "d3-scale";
 import { interpolateRdYlGn } from "d3-scale-chromatic";
+import styles from "./flat-drawing-helper.module.css";
 
 const path = d3Line()
   .x(d => d.x)
@@ -34,15 +36,17 @@ export const getDrawFunction = ({ config }) => ({ container, map, data }) => {
   const feature = container.selectAll(".flat").data(
     data.map(item => {
       const { x, y } = map.latLngToLayerPoint([item.latitude, item.longitude]);
+      const radius = sizeScale(item.size);
       return {
         ...item,
         x,
         y,
+        radius,
         polygon: createNgon({
           x,
           y,
           sides: item.rooms + 1,
-          radius: sizeScale(item.size),
+          radius,
           rotation: 270
         })
       };
@@ -50,20 +54,40 @@ export const getDrawFunction = ({ config }) => ({ container, map, data }) => {
     d => d.id
   );
 
-  const addOrUpdateFeature = feature => {
-    feature
-      .attr("d", d => path(d.polygon))
-      .style("fill", d => sqMPriceScale(d.price / d.size))
-      .style("opacity", 0.8);
+  const addOrUpdatePath = feature => {
+    feature.attr("d", d => (d.rooms === 1 ? null : path(d.polygon)));
+  };
+
+  const addOrUpdateCircle = feature => {
+    feature.attr("cx", d => (d.rooms === 1 ? d.x : null));
+    feature.attr("cy", d => (d.rooms === 1 ? d.y : null));
+    feature.attr("r", d => (d.rooms === 1 ? d.radius : null));
+  };
+
+  const addOrUpdate = feature => {
+    addOrUpdateCircle(feature);
+    addOrUpdatePath(feature);
+    feature.style("fill", d => sqMPriceScale(d.price / d.size));
+    feature.on("mouseover", function() {
+      d3Select(this).classed(styles.flatmarkerHovered, true);
+    });
+    feature.on("mouseout", function() {
+      d3Select(this).classed(styles.flatmarkerHovered, false);
+    });
   };
 
   feature
     .enter()
-    .append("path")
-    .attr("class", "flat")
-    .call(addOrUpdateFeature);
+    .append(d =>
+      document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        d.rooms === 1 ? "circle" : "path"
+      )
+    )
+    .attr("class", `flat ${styles.flatmarker}`)
+    .call(addOrUpdate);
 
   feature.exit().remove();
 
-  feature.call(addOrUpdateFeature);
+  feature.call(addOrUpdate);
 };
