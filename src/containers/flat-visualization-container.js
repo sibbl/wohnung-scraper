f75f9776b06bef745c3c345ca02d0fbc80bccc4b
Sebastian Müller <sebastian.mueller@we-do.ai>
2019-10-18 13:50:12 +0200

    Show flat count or current flat title in document title

--- src/containers/flat-visualization-container.js
+++ src/containers/flat-visualization-container.js
@@ -1,16 +1,46 @@
 import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { connect } from "react-redux";
 import { FlatVisualization } from "../components/flat-visualization";
 import { LoadingView } from "../components/loading-view";
 import { getConfig } from "../actions/config-actions";
 import { getFlats } from "../actions/flat-actions";
 
-const FlatVisualizationContainer = ({ config, flats, getFlats, getConfig }) => {
+const FlatVisualizationContainer = ({
+  config,
+  flats,
+  getFlats,
+  getConfig,
+  visibleFlatIds,
+  selectedFlatId
+}) => {
   useEffect(() => {
     getConfig();
     getFlats();
   }, []);
-  return config && flats ? <FlatVisualization /> : <LoadingView />;
+  return config && flats ? (
+    <React.Fragment>
+      <Helmet>
+        <title>
+          Wohnung Scraper -{" "}
+          {getTitleSuffix(flats, selectedFlatId, visibleFlatIds)}
+        </title>
+      </Helmet>
+      <FlatVisualization />
+    </React.Fragment>
+  ) : (
+    <LoadingView />
+  );
+};
+
+const getTitleSuffix = (flats, selectedFlatId, visibleFlatIds) => {
+  if (selectedFlatId) {
+    return flats[selectedFlatId].title;
+  } else {
+    return (
+      visibleFlatIds.length + (visibleFlatIds.length == 1 ? " flat" : " flats")
+    );
+  }
 };
 
 export default connect(
@@ -21,7 +51,10 @@ export default connect(
 
     flat_isWorking: state.flat.isWorking,
     flat_error: state.flat.error,
-    flats: state.flat.flats
+    flats: state.flat.flats,
+
+    visibleFlatIds: state.flat.visibleFlatIds,
+    selectedFlatId: state.flat.selectedFlatId
   }),
   dispatch => ({
     getConfig: () => dispatch(getConfig()),
