04bb9a9fdd01055014dfc29ea16b1eb97f0b39b3
Sebastian Schubotz <github@sibbl.net>
2023-06-11 13:06:54 +0200

    Add mobile layout for map + sidebar

--- src/components/flat-visualization.js
+++ src/components/flat-visualization.js
@@ -2,11 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import FlatMapContainer from "../containers/flat-map-container";
 import { Sidebar } from "./sidebar";
+import { MdExpandLess, MdExpandMore } from "react-icons/md";
+import { useState } from "react";
 
 const StyledFlatVisualization = styled.div`
   display: flex;
   flex-flow: row nowrap;
   height: 100%;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const StyledFlatMapContainer = styled(FlatMapContainer)`
@@ -14,9 +19,33 @@ const StyledFlatMapContainer = styled(FlatMapContainer)`
   flex: 1;
 `;
 
-export const FlatVisualization = ({ ...other }) => (
-  <StyledFlatVisualization {...other}>
-    <StyledFlatMapContainer />
-    <Sidebar />
-  </StyledFlatVisualization>
-);
+const StyledSidebar = styled(Sidebar)`
+  width: 320px;
+  @media (max-width: 768px) {
+    width: 100%;
+    height: ${props => props.isExpanded ? '80vh' : '33vh'};
+  }
+`;
+
+const StyledExtendButton = styled.button`
+  border: 0;
+  background: #eee;
+  width: 100%;
+  padding: 6px 0;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const FlatVisualization = ({ ...other }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <StyledFlatVisualization {...other}>
+      <StyledFlatMapContainer />
+      <StyledExtendButton onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? <MdExpandMore /> : <MdExpandLess />}
+      </StyledExtendButton>
+      <StyledSidebar isExpanded={isExpanded} />
+    </StyledFlatVisualization>
+  );
+};
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,15 +6,14 @@ import SelectedFlatDetailsContainer from "../containers/selected-flat-details-co
 import SidebarPublicTransportPanelContainer from "../containers/sidebar-public-transport-panel-container";
 
 const StyledSidebar = styled.div`
-  width: 320px;
   padding: 16px;
   overflow-x: hidden;
   overflow-y: auto;
 `;
 
-export const Sidebar = () => {
+export const Sidebar = ({...others}) => {
   return (
-    <StyledSidebar>
+    <StyledSidebar {...others}>
       <SelectedFlatDetailsContainer />
       <AccordionItem title="Public Transport">
         <SidebarPublicTransportPanelContainer />
